c7c9eb20b7ed4b3b817f5ad713333b52d8ae928a
flaviomiller <flaviomiller@icloud.com>
2023-02-07 14:23:40 -0300

    Revert "Create cypress project"
    This reverts commit 9da7687a58cd92332dc14c103388208eb063dc38.

--- cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function(){
-   cy.get('#firstName').type('Flavio')
-   cy.get('#lastName').type('Miller')
-   cy.get('#email').type('flaviomiller@teste.com')
-   cy.get('#open-text-area').type('Hello World')
-   cy.get('button[type="submit"]').click()
-})
